b003e8192d6ed5018443d9ff98f72a3ab97ae010
Arik Sosman <arik@bitgo.com>
2018-05-06 09:15:29 -0700

    separate tests

--- test/wallet_test.go
+++ test/wallet_test.go
@@ -6,20 +6,6 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
-func TestGetWallet(t *testing.T) {
-  bitgo := src.BitGoSDK("http://localhost", 3080)
-  bitgo.Authenticate("arik+test007@bitgo.com", "test@bitgo.com", "0000000")
-  wallet := bitgo.Coin("tltc").Wallets().Get("5984e0b26b423fda07a5d6d2d226c87a")
-  assert.Equal(t, "5984e0b26b423fda07a5d6d2d226c87a", wallet.Id())
-}
-
-func TestListWallet(t *testing.T) {
-  bitgo := src.BitGoSDK("http://localhost", 3080)
-  bitgo.Authenticate("arik+test007@bitgo.com", "test@bitgo.com", "0000000")
-  wallets := bitgo.Coin("tltc").Wallets().List()
-  assert.Equal(t, 14, len(wallets))
-}
-
 func TestSendMany(t *testing.T) {
   bitgo := src.BitGoSDK("http://localhost", 3080)
   bitgo.Authenticate("arik+test007@bitgo.com", "test@bitgo.com", "0000000")
